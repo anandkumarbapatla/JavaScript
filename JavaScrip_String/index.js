707e21816dc1cd2f791ea10b9f13d03afe652bf2
anandkumarbapatla <anandkumar1702@gmail.com>
2022-11-04 09:19:55 -0500

    JavaScript Strings
    Strings basics

---
+++ JavaScrip_String/index.js
@@ -0,0 +1,9 @@
+let userName = "Anand Kumar";
+
+console.log(userName.length);
+console.log(userName.charAt(0));
+console.log(userName.indexOf('A'));
+console.log(userName.lastIndexOf('a'));
+console.log(userName.trim());
+console.log(userName.toUpperCase());
+console.log(userName.toLowerCase());
